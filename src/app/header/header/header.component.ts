355a6a90a6e1d568f9477c1373d6b24a8ec889f6
David <dguillen12344321@gmail.com>
2022-09-07 23:19:59 -0300

    fix

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardService } from 'src/app/services/board.service';
+import { GoogleauthService } from 'src/app/services/googleauth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  
+  GoogleauthService: GoogleauthService;
+
   constructor(
     public boardService: BoardService
   ) { }
-
+  async onGoogleLogin() {
+    try {
+      this.GoogleauthService.loginGoogle();
+    } catch (error) { console.log(error) }
+  }
   ngOnInit(): void {
   }
 
---
+++ src/app/services/googleauth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleauthService {
+
+  constructor(public afAuth: AngularFireAuth) { }
+  async loginGoogle() {
+    try {
+      return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
